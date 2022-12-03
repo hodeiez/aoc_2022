873f481dabcc18271918f9dce298d8b92ec83ea0
hodeiez <hodeinaiz@hotmail.com>
2022-12-03 14:16:13 +0100

    another dirty part2

--- day3/day3.java
+++ day3/day3.java
@@ -2,11 +2,13 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class day3 {
 
@@ -17,10 +19,32 @@ public class day3 {
         List<String> stringList = Files.readAllLines(file);
         String[] inputArr = stringList.toArray(new String[] {});
 
+        var inputList = Arrays.stream(inputArr).collect(Collectors.toList());
         // part1
         System.out.println(
-                Arrays.stream(inputArr).collect(Collectors.toList()).stream().mapToInt(a -> getValue(find(a))).sum());
+                inputList.stream().mapToInt(a -> getValue(find(a))).sum());
 
+        // part2
+        System.out.println(IntStream.range(0, (inputList.size() + 2) / 3)
+                .mapToObj(i -> inputList.subList(i * 3, Math.min(3 * (i + 1),
+                        inputList.size())))
+                .collect(Collectors.toList()).stream().mapToInt(a -> findHelper(a)).sum());
+
+    }
+
+    public static int findHelper(List<String> lines) {
+        return getValue(find2(lines.get(2), find2(lines.get(0), lines.get(1))));
+    }
+
+    public static String find2(String line1, String line2) {
+
+        Pattern path = Pattern.compile("[" + line1 + "]");
+        Matcher matcher = path.matcher(line2);
+        List<String> temp = new ArrayList<String>();
+        while (matcher.find()) {
+            temp.add(matcher.group());
+        }
+        return temp.stream().map(Object::toString).collect(Collectors.joining(""));
     }
 
     public static String find(String line) {
